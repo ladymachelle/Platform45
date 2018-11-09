88cbd08c7d283a67db609c8ded0b1de64b9e3810
Machelle <machelleroeloffze@gmail.com>
2018-11-09 08:06:57 +0200

    latest

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,54 +1,130 @@
+//clear form
+
 function clearForm() {
   document.getElementById("form").reset();
 }
 
-function expand() {
-  var form = document.getElementById("form");
-  var blob = document.getElementById("blob");
-}
-
-function collapse() {
-  var form = document.getElementById("form");
-  var blob = document.getElementById("blob");
-}
+//Slider toggle
 
 function toggle() {
-  var form = document.querySelector("slide_panel");
-  var list = Object.values("form.classList");
-  var button = document.querySelector("user_button");
+  var form = document.querySelector("[class*=slide_panel]");
+  var button = document.querySelector("button[class*=user_button]");
 
-  list.map(x => {
-    if (x === "slide") {
-      form.classList.remove("slide");
-      button.classList.remove("user_button_rotate");
+  if (form.classList.contains("slide")) {
+    form.classList.remove("slide");
+    button.classList.remove("user_button_rotate");
+  } else {
+    form.classList.add("slide");
+    button.classList.add("user_button_rotate");
+  }
+}
+
+//Form validation
+
+let inputs = document.querySelectorAll("input");
+let errMsgContainer = document.createElement("span");
+
+inputs.forEach(function(el) {
+  console.log(el);
+
+  el.addEventListener("blur", function(evt) {
+    if (this.value == "null" || this.value == "undefined" || this.value == "") {
+      switch (el.name) {
+        case "name":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please provide name.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+        case "gender":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please select your gender.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+        case "date":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please provide date of birth.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+
+        case "email":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please provide valid email address.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+        case "mobile":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please provide mobile number.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+        case "membership":
+          el.parentNode
+            .appendChild(errMsgContainer)
+            .classList.add("form_input__error_message");
+          errMsgContainer.innerHTML = "Please select a membership option.";
+          el.classList.add("form_input__error");
+          el.previousSibling.previousSibling.classList.add("form_label__error");
+          break;
+      }
     } else {
-      form.classList.add("slide");
-      button.classList.add("user_button_rotate");
+      switch (el.name) {
+        case "name":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+        case "gender":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+        case "date":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+        case "email":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+        case "mobile":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+        case "membership":
+          el.parentNode.removeChild(errMsgContainer);
+          el.classList.remove("form_input__error");
+          el.previousSibling.previousSibling.classList.remove(
+            "form_label__error"
+          );
+          break;
+      }
     }
   });
-}
-// function validate() {
-//   if (document.form.name.value == "") {
-//     alert("Please provide your name!");
-//     document.form.name.focus();
-//     return false;
-//   }
-
-//   if (document.form.email.value == "") {
-//     document.getElementById("error").innerHTML = "error here";
-//     document.form.email.focus();
-//     return false;
-//   }
-
-//   if (
-//     document.form.mobile.value == "" ||
-//     isNaN(document.form.mobile.value) ||
-//     document.form.mobile.value.length != 12
-//   ) {
-//     alert("Please provide a valid mobile number.");
-//     document.form.mobile.focus();
-//     return false;
-//   }
-
-//   return true;
-// }
+});
